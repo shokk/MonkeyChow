b3bc4e59475bdc7e882ac05450c05f7f7c7f6271
Ernie Oporto <ernieoporto@yahoo.com>
2013-06-02 13:05:09 -0400

    updated frames

--- rss2.php
+++ rss2.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once("init.php");
 include_once("rss10.inc");
 
@@ -9,12 +8,16 @@ $rss =new RSSWriter(
 	$REPUBLISH_CHANNEL_DESC
 );
 
-$sql="select '$FOF_FEED_TABLE.tags' as 'feed_tags', 'items.read' as 'item_read', '$FOF_FEED_TABLE.title' as 'feed_title', '$FOF_FEED_TABLE.link' as 'feed_link', $FOF_FEED_TABLE.description as feed_description, items.id as item_id, items.link as item_link, items.title as item_title, UNIX_TIMESTAMP(items.timestamp) as timestamp, items.content as item_content, items.dcdate as dcdate, items.dccreator as dccreator, items.dcsubject as dcsubject, items.publish as item_publish, items.star as item_star from $FOF_FEED_TABLE, items where items.feed_id=$FOF_FEED_TABLE.id and $FOF_FEED_TABLE.private=0";
+$sql="select '$FOF_FEED_TABLE.tags' as 'feed_tags', '$FOF_ITEM_TABLE.read' as 'item_read', '$FOF_FEED_TABLE.title' as 'feed_title', '$FOF_FEED_TABLE.link' as 'feed_link', $FOF_FEED_TABLE.description as feed_description, $FOF_ITEM_TABLE.id as item_id, $FOF_ITEM_TABLE.link as item_link, $FOF_ITEM_TABLE.title as item_title, UNIX_TIMESTAMP($FOF_ITEM_TABLE.timestamp) as timestamp, $FOF_ITEM_TABLE.content as item_content, $FOF_ITEM_TABLE.dcdate as dcdate, $FOF_ITEM_TABLE.dccreator as dccreator, $FOF_ITEM_TABLE.dcsubject as dcsubject, $FOF_ITEM_TABLE.publish as item_publish, $FOF_ITEM_TABLE.star as item_star from $FOF_FEED_TABLE, $FOF_ITEM_TABLE where $FOF_ITEM_TABLE.feed_id=$FOF_FEED_TABLE.id and $FOF_FEED_TABLE.private=0";
+#$flag_sql = "SELECT blah FROM `" . $FOF_USERITEM_TABLE . "` WHERE `flag_id`="3" AND `item_id`=" . $item_id . " AND `user_id`=" . current_user();
 
 $mytags = mysql_escape_string($_REQUEST['tags']);
 $sql .= ($_REQUEST['tags']) ? " and $FOF_FEED_TABLE.tags LIKE \"%" . $mytags . "%\"" : "";
 
 $sql.=" order by timestamp desc limit 200";
+
+print $sql . "<br/>\n";
+exit;
 $result = fof_do_query($sql);
 
 $timestamp;
